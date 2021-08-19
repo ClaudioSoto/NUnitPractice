a54ee54f0e1cbb0eeb5eabf0ec2d3b9d3158d4bc
Claudio Soto <APIcourseLCSA@gmail.com>
2021-08-19 14:49:48 -0500

    EmployeeController exercise done with refactoring, dependency injection and tests

--- TestNinja/TestNinja/Mocking/EmployeeController.cs
+++ TestNinja/TestNinja/Mocking/EmployeeController.cs
@@ -11,11 +11,14 @@ namespace TestNinja.Mocking
             _db = new EmployeeContext();
         }
 
-        public ActionResult DeleteEmployee(int id)
+        public ActionResult DeleteEmployee(int id, IEmployeeStorage employeeStorage)
         {
+            /* THIS CODE WAS SENT TO EmployeeStorage class
             var employee = _db.Employees.Find(id);
             _db.Employees.Remove(employee);
             _db.SaveChanges();
+            */
+            employeeStorage.DeleteEmployee(id);
             return RedirectToAction("Employees");
         }
 
