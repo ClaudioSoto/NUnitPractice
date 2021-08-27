0e07b71fa8c10305ba9d1160c4d731f31e1b5566
Claudio Soto <APIcourseLCSA@gmail.com>
2021-08-27 14:45:33 -0500

    HtmlFormatter tests done

---
+++ TestNinja/TestNinja.Tests/Fundamentals/HtmlFormatterTests.cs
@@ -0,0 +1,43 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using TestNinja.Fundamentals;
+
+namespace TestNinja.Tests.Fundamentals
+{
+    [TestFixture]
+    class HtmlFormatterTests
+    {
+        private HtmlFormatter _htmlFormatterClassObject;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _htmlFormatterClassObject = new HtmlFormatter();
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            _htmlFormatterClassObject = null;
+        }
+
+        [Test]
+        [TestCase("a")]
+        [TestCase("A")]
+        [TestCase("aA")]
+        [TestCase("Aa")]
+        public void FormatAsBold_UseStrongLabels_ReturnBoldHtmlString(string word)
+        {
+            //Act
+            var result = _htmlFormatterClassObject.FormatAsBold(word);
+
+            //Arrange
+            Assert.That(result, Does.StartWith("<strong>"));
+            Assert.That(result, Does.Contain(word).IgnoreCase);
+            Assert.That(result, Does.EndWith("</strong>"));
+        }
+
+    }
+}
