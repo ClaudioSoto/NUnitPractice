91b70881db1a52e6c8d921b071079586e29bd142
Claudio Soto <APIcourseLCSA@gmail.com>
2021-08-27 12:53:23 -0500

    CustomerController tests done

---
+++ TestNinja/TestNinja.Tests/Fundamentals/CustomerControllerTests.cs
@@ -0,0 +1,38 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using TestNinja.Fundamentals;
+
+namespace TestNinja.Tests.Fundamentals
+{
+    [TestFixture]
+    class CustomerControllerTests
+    {
+        [Test]
+        public void GetCustomer_WhenIdIsZero_ReturnNotFound()
+        {
+            //Arrange
+            var customerControllerClassObject = new CustomerController();
+
+            //Act
+            var result = customerControllerClassObject.GetCustomer(0);
+
+            //Assert
+            Assert.That(result, Is.InstanceOf<NotFound>());
+        }
+
+        [Test]
+        public void GetCustomer_WhenIdIsNotZero_ReturnOk()
+        {
+            //Arrange
+            var customerControllerClassObject = new CustomerController();
+
+            //Act
+            var result = customerControllerClassObject.GetCustomer(1);
+
+            //Assert
+            Assert.That(result, Is.InstanceOf<Ok>());
+        }
+    }
+}
