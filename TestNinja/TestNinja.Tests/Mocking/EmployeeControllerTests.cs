05fa4f9e353112258deabb72e81228a0a81b4be5
Claudio Soto <APIcourseLCSA@gmail.com>
2021-08-30 13:35:22 -0500

    EmployeeController tests done

---
+++ TestNinja/TestNinja.Tests/Mocking/EmployeeControllerTests.cs
@@ -0,0 +1,27 @@
+﻿using Moq;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using TestNinja.Mocking;
+
+namespace TestNinja.Tests.Mocking
+{
+    [TestFixture]
+    class EmployeeControllerTests
+    {
+        [Test]
+        public void DeleteEmployee_WhenCalled_DeleteEmployeeFromDb()
+        {
+            //Arrange
+            var mockEmployeeStorage = new Mock<IEmployeeStorage>();
+            var employeeControllerClassObject = new EmployeeController(mockEmployeeStorage.Object);
+
+            //Act
+            employeeControllerClassObject.DeleteEmployee(1);
+
+            //Assert
+            mockEmployeeStorage.Verify(mes => mes.deleteEmployee(1));
+        }
+    }
+}
---
+++ TestNinja/TestNinja/Mocking/EmployeeStorage.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestNinja.Mocking
+{
+    public interface IEmployeeStorage
+    {
+        void deleteEmployee(int id);
+    }
+
+    public class EmployeeStorage : IEmployeeStorage
+    {
+        private EmployeeContext _db;
+        public void deleteEmployee(int id)
+        {
+            var employee = _db.Employees.Find(id);
+            _db.Employees.Remove(employee);
+            _db.SaveChanges();
+        }
+    }
+}
