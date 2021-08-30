16688712c2718fda38af4e816f4e13690e0ee432
Claudio Soto <APIcourseLCSA@gmail.com>
2021-08-30 16:49:53 -0500

    Product tests done

---
+++ TestNinja/TestNinja.Tests/Fundamentals/ProductTests.cs
@@ -0,0 +1,46 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using TestNinja.Mocking;
+
+namespace TestNinja.Tests.Fundamentals
+{
+    [TestFixture]
+    class ProductTests
+    {
+        private Product _productClassObject;
+        private Customer _customerAuxObject;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _productClassObject = new Product();
+            _customerAuxObject = new Customer();
+
+            _productClassObject.ListPrice = 1;
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            _productClassObject = null;
+            _customerAuxObject = null;
+        }
+
+        [Test]
+        [TestCase(true,0.7f)]
+        [TestCase(false, 1)]
+        public void GetPrice_WhenCalled_ReturnCalculation(bool isGold, float expectedValue)
+        {
+            //Arrange
+            _customerAuxObject.IsGold = isGold;
+
+            //Act
+            var result = _productClassObject.GetPrice(_customerAuxObject);
+
+            //Assert
+            Assert.That(result, Is.EqualTo(expectedValue));
+        }
+    }
+}
